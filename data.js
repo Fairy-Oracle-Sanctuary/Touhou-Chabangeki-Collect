550df521c7920444254fc8b526dfe5d670811511
baby20162016 <2185823427@qq.com>
2026-02-03 12:34:57 +0800

    Update data

--- data.js
+++ data.js
@@ -6,12 +6,37 @@ const dramas = [
         translator: "就是很一般",
         tags: ["小町","圣白莲","命莲","八意永琳","射命丸文","琪露诺","大妖精","多多良小伞"],
         isTranslated: true,
-        originalUrl: "https://www.youtube.com/watch?v=Zth3g7mHbp0&t=305s",
+        originalUrl: "https://www.youtube.com/playlist?list=PLxioisTzjlBa1NoKDTH1nkAHgOwAHzhma",
         translatedUrl: "https://space.bilibili.com/12621850/lists/4882608",
-        description: "海上小岛怪谈故事。",
-        // thumbnail: "https://img.remit.ee/api/file/BQACAgUAAyEGAASHRsPbAAEQpZZpgG2plLStQynYfxcTrlOQywABP24AAgUdAAKqLghUpepPPzAbNbs4BA.jpg",
-        thumbnail: "https://gitee.com/Fairy-Oracle-Sanctuary/touhou-chabangeki-collect-cover/raw/main/cover/1.jpg",
+        description: "你是我活过的证明。",
+        thumbnail: "https://img.remit.ee/api/file/BQACAgUAAyEGAASHRsPbAAEQpZZpgG2plLStQynYfxcTrlOQywABP24AAgUdAAKqLghUpepPPzAbNbs4BA.jpg",
         dateAdded: "2023-06-18"
+    },
+    {
+        id: 2,
+        title: "幻想死洛谭",
+        author: "ささきの茶釜",
+        translator: "就是很一般",
+        tags: [],
+        isTranslated: true,
+        originalUrl: "https://www.youtube.com/playlist?list=PLxioisTzjlBZ63LCFxhe0uDIma4lgCMfC",
+        translatedUrl: "https://space.bilibili.com/12621850/lists/5066012",
+        description: "死而未绝，络续不绝之缘。",
+        thumbnail: "https://img.remit.ee/api/file/BQACAgUAAyEGAASHRsPbAAEQqOlpgXfhLwyRXfUQpXQ4KX38jKPCyAACaR0AAqouEFSZ0O7DS7DlHzgE.jpg",
+        dateAdded: "2024-08-24"
+    },
+    {
+        id: 3,
+        title: "幻想心狼殿",
+        author: "ささきの茶釜",
+        translator: "就是很一般",
+        tags: [],
+        isTranslated: true,
+        originalUrl: "https://www.youtube.com/playlist?list=PLxioisTzjlBaXcAkHQes8-dkINhSlG-Aj",
+        translatedUrl: "https://space.bilibili.com/12621850/lists/6561665",
+        description: "一群变态似乎在拯救世界。",
+        thumbnail: "https://img.remit.ee/api/file/BQACAgUAAyEGAASHRsPbAAEQqOppgXgeKPti5matgwUJFx2qzjWj0wACax0AAqouEFRIAzAUCiA6MTgE.jpg",
+        dateAdded: "2025-08-17"
     },
 
 ];
